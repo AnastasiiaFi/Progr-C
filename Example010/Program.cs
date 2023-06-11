404a22af1540964d7961a689a0fcc15b80f55190
AnastasiaK <Fair996@mail.ru>
2023-06-11 15:25:17 +0500

    Example010

---
+++ Example010/Program.cs
@@ -0,0 +1,52 @@
+﻿//Массивы, продолжение
+
+void FillArray(int[] collection) //Метод формирования чисел случайным образом
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col) //Метод вывода числе
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        System.Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[]collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; //Принятое обозначение, чтобы если нет числа, то выдавалось -1, а не 0 позиция.
+
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index; 
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+array[4] = 4;
+array[4] = 4;
+PrintArray(array); 
+System.Console.WriteLine();
+
+int pos = IndexOf(array, 44);
+System.Console.WriteLine(pos);
